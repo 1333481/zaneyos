343e37156923522af437fe5c981fb683cb61b90d
Don Williams <don.e.williams@gmail.com>
2026-01-15 10:01:15 -0500

    Getting noctalia to update on rebuilds/updates'

--- modules/home/noctalia.nix
+++ modules/home/noctalia.nix
@@ -3,13 +3,11 @@
   inputs,
   lib,
   ...
-}:
-let
+}: let
   system = pkgs.stdenv.hostPlatform.system;
   noctaliaPkg = inputs.noctalia.packages.${system}.default;
   configDir = "${noctaliaPkg}/share/noctalia-shell";
-in
-{
+in {
   # Install the Noctalia package
   home.packages = [
     noctaliaPkg
@@ -17,12 +15,13 @@ in
   ];
 
   # Seed the configuration
-  home.activation.seedNoctaliaShellCode = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+  home.activation.seedNoctaliaShellCode = lib.hm.dag.entryAfter ["writeBoundary"] ''
     set -eu
     DEST="$HOME/.config/quickshell/noctalia-shell"
     SRC="${configDir}"
 
     if [ ! -d "$DEST" ]; then
+      $DRY_RUN_CMD rm -rf "$DEST"
       $DRY_RUN_CMD mkdir -p "$HOME/.config/quickshell"
       $DRY_RUN_CMD cp -R "$SRC" "$DEST"
       $DRY_RUN_CMD chmod -R u+rwX "$DEST"
