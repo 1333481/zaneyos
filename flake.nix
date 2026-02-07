8930bdcefbbd931f1d925530525543eb81d06036
Jesse Stoltz <jessestoltz07@gmail.com>
2026-02-07 14:37:05 -0600

    Updated Monitor Configs

--- flake.nix
+++ flake.nix
@@ -47,9 +47,9 @@
     ...
   } @ inputs: let
     system = "x86_64-linux";
-    host = "zaneyos-24-vm";
-    profile = "vm";
-    username = "dwilliams";
+    host = "nixos-laptop";
+    profile = "intel";
+    username = "jessestoltz";
 
     # Deduplicate nixosConfigurations while preserving the top-level 'profile'
     mkNixosConfig = gpuProfile:
